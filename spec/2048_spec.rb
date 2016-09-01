c0de24ebc026925df2389469f1b8414e30a4a262
Josh Cheek <josh.cheek@gmail.com>
2016-09-01 12:41:37 -0500

    Big high-level test

---
+++ spec/2048_spec.rb
@@ -0,0 +1,78 @@
+RSpec.describe '2048 bot' do
+  def bot_for(board)
+    Game::Bot.new board
+  end
+
+  it 'can shift left' do
+    board = Game::Board.new [
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [2, 2, 0, 0],
+    ]
+    expect(bot_for(board).move).to eq :left
+    expect(board.shift(:left).to_a).to eq [
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [4, 0, 0, 0],
+    ]
+  end
+
+  it 'can shift down' do
+    board = Game::Board.new [
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [2, 0, 0, 0],
+      [2, 0, 0, 0],
+    ]
+    expect(bot_for(board).move).to eq :down
+    expect(board.shift(:down).to_a).to eq [
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [4, 0, 0, 0],
+    ]
+  end
+
+  it 'can shift right' do
+    board = Game::Board.new [
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 2, 2],
+      [0, 0, 0, 0],
+    ]
+    expect(bot_for(board).move).to eq :right
+    expect(board.shift(:right).to_a).to eq [
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 4],
+      [0, 0, 0, 0],
+    ]
+  end
+
+  it 'can shift up' do
+    board = Game::Board.new [
+      [0, 0, 2, 0],
+      [0, 0, 2, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+    ]
+    expect(bot_for(board).move).to eq :up
+    expect(board.shift(:up).to_a).to eq [
+      [0, 0, 4, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+    ]
+  end
+
+  it 'shifts in such a way that it combines tiles for as long as possible' do
+    board = Game::Board.random_start
+    100.times do
+      bot   = bot_for(board)
+      board = board.shift(bot.move)
+    end
+    expect(board.max_tile).to be >= 128
+  end
+end
