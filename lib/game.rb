4569494b631c654ca03ed94f362862c4bdbe6a78
Josh Cheek <josh.cheek@gmail.com>
2016-09-01 15:48:25 -0500

    Simple bot heuristic

--- lib/game.rb
+++ lib/game.rb
@@ -155,19 +155,30 @@ module Game
 
     # maybe the heuristic should be injectable?
     def heuristic(board)
-      # fewer tiles is better
-      # tiles with higher numbers are worth more
-      # corners are better
-      # sequences of continually ascending / descending are better than flipping direction
+      # Maybe ultimately consider these:
+      #   fewer tiles is better
+      #   tiles with higher numbers are worth more
+      #   corners are better
+      #   sequences of continually ascending / descending are better than flipping direction
+      # But for now, just something super simple
+      score = 0
 
-      sum = 0
+      # larger tiles are worth more points
       for y in 0..3
-        # for now, just something super simple
-        for x in 0..4
-          sum += (2**board[y, x])
+        for x in 0..3
+          score += (2**board[y, x])
         end
       end
-      sum
+
+      # tiles along the edge are worth more points
+      # and prefer the larger tiles be along more edges
+      (0..3).each do |edge|
+        score += board[0, edge]
+        score += board[3, edge]
+        score += board[edge, 0]
+        score += board[edge, 3]
+      end
+      score
     end
   end
 end
