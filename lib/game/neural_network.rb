9e30ffa4db4e563167f4a5498a80d17749641d74
Josh Cheek <josh.cheek@gmail.com>
2016-09-02 03:29:11 -0500

    Use the word "synapses" over the word "weights"

--- lib/game/neural_network.rb
+++ lib/game/neural_network.rb
@@ -17,11 +17,11 @@
 # Most of the changes take the form of introducing immutability (at least from the outside),
 # and pushing variable setting / modifying earlier in the callstack.
 class NeuralNetwork
-  attr_reader :layer_sizes, :weights
+  attr_reader :layer_sizes, :synapses
 
-  def initialize(layer_sizes, weights)
+  def initialize(layer_sizes, synapses)
     self.layer_sizes = layer_sizes
-    self.weights     = weights
+    self.synapses    = synapses
   end
 
   def eval(input_values)
@@ -30,14 +30,14 @@ class NeuralNetwork
 
   def neurons_for(input_values)
     neurons = [input_values.dup]
-    neurons.concat weights.map { |w| Array.new w.first.length, 0 }
+    neurons.concat synapses.map { |w| Array.new w.first.length, 0 }
     neurons[0...-1].each { |layer| layer << 1.0 }
     neurons
   end
 
   def feedforward(input_values)
     neurons = neurons_for(input_values)
-    weights.each_with_index do |layer_weights, layer|
+    synapses.each_with_index do |layer_weights, layer|
       layer_sizes[layer+1].times do |tndx|
         sum = neurons[layer].each_index.reduce(0.0) do |sum, fndx|
           sum + (neurons[layer][fndx] * layer_weights[fndx][tndx])
@@ -56,19 +56,19 @@ class NeuralNetwork
 
   private
 
-  attr_writer :layer_sizes, :weights
+  attr_writer :layer_sizes, :synapses
 end
 
 class NeuralNetwork
   class Trainer
     def self.init(layer_sizes:, learning_rate:, momentum:, random: Random)
-      weights = random_weights_for(layer_sizes, random)
-      network = NeuralNetwork.new(layer_sizes, weights)
-      changes = initial_changes(weights)
+      synapses = random_weights_for(layer_sizes, random)
+      network  = NeuralNetwork.new(layer_sizes, synapses)
+      changes  = initial_changes(synapses)
       new(network, changes, learning_rate, momentum)
     end
 
-    # weights indexes: from_layer, from_node_weights, to_node_weight
+    # synapses indexes: from_layer, from_node_weights, to_node_weight
     def self.random_weights_for(layer_sizes, random)
       layer_sizes.each_cons(2).map do |origin, target|
         Array.new(origin+1) do # 1 for the bias node
@@ -77,10 +77,10 @@ class NeuralNetwork
       end
     end
 
-    def self.initial_changes(weights)
-      Array.new(weights.length) do |w|
-        Array.new(weights[w].length) do |i|
-          Array.new(weights[w][i].length, 0.0)
+    def self.initial_changes(synapses)
+      Array.new(synapses.length) do |w|
+        Array.new(synapses[w].length) do |i|
+          Array.new(synapses[w][i].length, 0.0)
         end
       end
     end
@@ -106,13 +106,13 @@ class NeuralNetwork
 
     public def backpropagate(expected_output_values, all_neurons)
       all_deltas    = calculate_deltas(all_neurons, expected_output_values)
-      weights       = network.weights
+      synapses      = network.synapses
       next_weights  = []
       next_changes  = []
 
-      (weights.length-1).downto(0) do |n|
+      (synapses.length-1).downto(0) do |n|
         # values for the current layer
-        lweights = weights[n]
+        lweights = synapses[n]
         ldeltas  = all_deltas[n]
         lneurons = all_neurons[n]
         lchanges = changes[n]
@@ -141,7 +141,7 @@ class NeuralNetwork
     end
 
     def calculate_deltas(neurons, expecteds)
-      all_weights = network.weights
+      all_weights   = network.synapses
       output_deltas = neurons.last.zip(expecteds).map do |actual, expected|
         network.d_propagation_fn(actual) * (expected - actual)
       end
@@ -150,9 +150,9 @@ class NeuralNetwork
         .downto(1)
         .reduce([output_deltas]) { |deltas, layer|
           deltas.unshift(
-            neurons[layer].zip(all_weights[layer]).map { |node, weights|
+            neurons[layer].zip(all_weights[layer]).map { |node, synapses|
               derivative = network.d_propagation_fn(node)
-              error      = weights.zip(deltas.first).map { |d, w| d * w }.reduce(0, :+)
+              error      = synapses.zip(deltas.first).map { |d, w| d * w }.reduce(0, :+)
               derivative * error
             }
           )
