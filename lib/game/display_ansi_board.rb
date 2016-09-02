30a829de8bcac4773c421c91d9e22bea2fac7b2f
Josh Cheek <josh.cheek@gmail.com>
2016-09-02 05:11:26 -0500

    Improve ANSI output
    * Darker yellow b/c it was dificult to see with f.lux
    * Fix column sizing
    * Use \r\n so that it prints correctly in raw mode, too

--- lib/game/display_ansi_board.rb
+++ lib/game/display_ansi_board.rb
@@ -11,25 +11,25 @@ module Game
     def call
       rows           = board.to_a
       cols           = rows.transpose.map { |col| col.map &:to_s }
-      max_width      = cols.flat_map(&:length).max
+      max_width      = cols.flat_map { |col| col.map &:length }.max
       widths         = cols.map { max_width }
       margin_size    = 15
-      horizontal     = BG + " "*widths.inject(margin_size, :+)+OFF+"\n"
+      horizontal     = BG + " "*widths.inject(margin_size, :+)+OFF+"\r\n"
       formats        = widths.map { |w| "%#{w}d" }
       formatted_rows = rows.map { |row|
         "#{BG}  " <<
           formats.zip(row).map do |format, tile|
             "#{colour(tile)} #{format % tile} "
           end.join("#{BG} ") <<
-          "#{BG}  #{OFF}\n"
+          "#{BG}  #{OFF}\r\n"
       }.join
       horizontal + formatted_rows + horizontal
     end
 
     private
 
-    OFF    = "\e[0m".freeze
-    BG     = "\e[39;48;5;243m".freeze
+    OFF = "\e[0m".freeze
+    BG  = "\e[39;48;5;243m".freeze
 
     attr_accessor :board
 
@@ -46,8 +46,8 @@ module Game
       when 16              then rgb(5,2,1)
       when 32              then rgb(5,1,1)
       when 64              then rgb(5,1,0)
-      when 128, 256        then rgb(5,4,1)
-      when 512, 1024, 2048 then rgb(5,4,0)
+      when 128, 256        then rgb(5,3,1)
+      when 512, 1024, 2048 then rgb(4,3,0)
       else                      "\e[1;38;5;251;48;5;236m"
       end
     end
